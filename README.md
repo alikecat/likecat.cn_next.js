db05f7d1e1545b24d08ad235d4d1da19e4efd98a
likecat <a@likecat.cn>
2024-02-16 23:31:21 +0800

    Add Hero section

--- README.md
+++ README.md
@@ -1,36 +1,9 @@
-This is a [Next.js](https://nextjs.org/) project bootstrapped with [`create-next-app`](https://github.com/vercel/next.js/tree/canary/packages/create-next-app).
+# Likecat.cn
 
-## Getting Started
+🚀 Greetings, cosmic wanderers! 🌌 Step into the cosmic construction site of my upcoming personal website – a digital universe in the making! 🌐 Currently, it's a canvas waiting for strokes of creativity and your stellar suggestions.
 
-First, run the development server:
+🌟 What cosmic wonders should this cyber sanctuary behold? That's where you come in! Your ingenious ideas can turn this digital playground into a constellation of captivating experiences – be it interactive marvels, mind-bending graphics, or a dedicated space for the weirdest cat videos in the cosmos.
 
-```bash
-npm run dev
-# or
-yarn dev
-# or
-pnpm dev
-# or
-bun dev
-```
+📧 Shoot your suggestions my way at a@likecat.cn, and let the brainstorming cosmic energy flow! Your imagination might just be the nebula that sparks this website's celestial transformation. 🎨💡
 
-Open [http://localhost:3000](http://localhost:3000) with your browser to see the result.
-
-You can start editing the page by modifying `app/page.tsx`. The page auto-updates as you edit the file.
-
-This project uses [`next/font`](https://nextjs.org/docs/basic-features/font-optimization) to automatically optimize and load Inter, a custom Google Font.
-
-## Learn More
-
-To learn more about Next.js, take a look at the following resources:
-
-- [Next.js Documentation](https://nextjs.org/docs) - learn about Next.js features and API.
-- [Learn Next.js](https://nextjs.org/learn) - an interactive Next.js tutorial.
-
-You can check out [the Next.js GitHub repository](https://github.com/vercel/next.js/) - your feedback and contributions are welcome!
-
-## Deploy on Vercel
-
-The easiest way to deploy your Next.js app is to use the [Vercel Platform](https://vercel.com/new?utm_medium=default-template&filter=next.js&utm_source=create-next-app&utm_campaign=create-next-app-readme) from the creators of Next.js.
-
-Check out our [Next.js deployment documentation](https://nextjs.org/docs/deployment) for more details.
+🙏 A galaxy-sized thank you for dropping by this construction zone. Your virtual hard hat and imaginative contributions are the stardust that propels this project forward! Brace yourself for the cosmic reveal – it's bound to be legendary! 🚧
--- app/page.tsx
+++ app/page.tsx
@@ -1,113 +1,79 @@
-import Image from "next/image";
-
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
+    <body>
+      <header>
+        <nav className="backdrop-brightness-75 backdrop-blur-md fixed w-full z-10">
+          <div className="container mx-auto px-4 flex justify-between">
+            <div className="flex flex-col py-2 text-center">
+              <span className="font-serif text-2xl font-bold tracking-wider">
+                Likecat
+              </span>
+              <span className="font-mono text-md">Bojun Zhang</span>
+            </div>
+            <div className="flex space-x-4 text-xl py-4">
+              <button className="p-2 rounded-md text-blue-200 hover:text-white hover:bg-slate-500/20 active:bg-slate-500/50 transition ease-in-out">
+                Home
+              </button>
+              <button className="p-2 rounded-md text-blue-200 hover:text-white hover:bg-slate-700 active:bg-slate-600 transition ease-in-out">
+                About
+              </button>
+            </div>
+          </div>
+        </nav>
+      </header>
+      <main>
+        <div className="bg-gradient-to-b from-sky-500 to-indigo-500">
+          <div
+            className="bg-cover"
+            style={{
+              backgroundImage:
+                "url('https://source.unsplash.com/random?wallpapers')",
+            }}
           >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+            <div className="bg-slate-900/50 backdrop-brightness-50">
+              <div className="container mx-auto px-4 pt-24 md:pt-28 xl:pt-32 pb-12 md:pb-16 xl:pb-20 font-mono xl:text-2xl md:text-xl xl:leading-loose md:leading-relaxed space-y-4">
+                <h1 className="font-serif xl:text-6xl lg:text-5xl md:text-4xl text-2xl font-bold tracking-widest mb-8">
+                  Likecat.cn
+                </h1>
+                <p>
+                  🚀 Greetings, cosmic wanderers! 🌌 Step into the cosmic
+                  construction site of my upcoming personal website – a digital
+                  universe in the making! 🌐 Currently, it’s a canvas waiting
+                  for strokes of creativity and your stellar suggestions.
+                </p>
+                <p>
+                  🌟 What cosmic wonders should this cyber sanctuary behold?
+                  That’s where you come in! Your ingenious ideas can turn this
+                  digital playground into a constellation of captivating
+                  experiences – be it interactive marvels, mind-bending
+                  graphics, or a dedicated space for the weirdest cat videos in
+                  the cosmos.
+                </p>
+                <p>
+                  📧 Shoot your suggestions my way at a@likecat.cn, and let the
+                  brainstorming cosmic energy flow! Your imagination might just
+                  be the nebula that sparks this website’s celestial
+                  transformation. 🎨💡
+                </p>
+                <p>
+                  🙏 A galaxy-sized thank you for dropping by this construction
+                  zone. Your virtual hard hat and imaginative contributions are
+                  the stardust that propels this project forward! Brace yourself
+                  for the cosmic reveal – it’s bound to be legendary! 🚧✨
+                </p>
+                <div className="space-x-4 flex">
+                  <button className="border rounded-md font-sans font-semibold px-4 py-2 hover:bg-slate-500/20 active:bg-slate-500/50 hover:scale-105 active:scale-110 transition ease-in-out">
+                    About Me
+                  </button>
+                  <button className="border rounded-md font-sans font-semibold px-4 py-2 hover:bg-slate-500/20 active:bg-slate-500/50 hover:scale-105 active:scale-110 transition ease-in-out">
+                    Mail Me
+                  </button>
+                </div>
+              </div>
+            </div>
+          </div>
         </div>
-      </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-full sm:before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50 text-balance`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+      </main>
+    </body>
   );
 }
